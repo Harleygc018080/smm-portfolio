2e63cb0fb9f8528df70c20299cad994364312232
Harley Carbonell <100908103+Harleygc018080@users.noreply.github.com>
2025-01-14 20:14:59 +0800

    welcomeSection.tsx

--- src/app/components/welcomeSection.tsx
+++ src/app/components/welcomeSection.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 
 const WelcomeSection = () => {
   return (
